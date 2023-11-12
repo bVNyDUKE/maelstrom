9c39c00749843f7fe3060f6bcc5c787973302864
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-11-12 21:32:47 +0100

    refactor

--- Makefile
+++ Makefile
@@ -4,8 +4,6 @@ DOCKER_CONTAINER_NAME := mlstrm-container
 APP_DIR := ./app
 GO_SRCS := $(wildcard $(APP_DIR)/*.go)
 
-w := echo
-
 $(APP_NAME): $(GO_SRCS)
 	cd $(APP_DIR) && go build -o ../$(APP_NAME)
 
@@ -16,10 +14,13 @@ run: build
 	docker run --name $(DOCKER_CONTAINER_NAME) -it $(DOCKER_IMAGE_NAME)
 
 test: build
-	docker run --name $(DOCKER_CONTAINER_NAME) -it --rm $(DOCKER_IMAGE_NAME) ./maelstrom test -w $(w) --bin mlstrm --nodes n1 --time-limit 10 --log-stderr
+	docker run --name $(DOCKER_CONTAINER_NAME) -it --rm $(DOCKER_IMAGE_NAME) ./maelstrom test -w echo --bin mlstrm --nodes n1 --time-limit 10 --log-stderr
 
 generate: build
 	docker run --name $(DOCKER_CONTAINER_NAME) -it --rm $(DOCKER_IMAGE_NAME) ./maelstrom test -w unique-ids --bin mlstrm --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+
+broadcast: build
+	docker run --name $(DOCKER_CONTAINER_NAME) -it --rm $(DOCKER_IMAGE_NAME) ./maelstrom test -w broadcast --bin mlstrm --time-limit 20 --rate 10 --node-count 1
 
 clean: 
 	rm -f $(APP_NAME)
--- app/node.go
+++ app/node.go
@@ -22,69 +22,81 @@ type MessageBody struct {
 	Echo      *string   `json:"echo,omitempty"`
 	NodeIds   *[]string `json:"node_ids,omitempty"`
 	Id        *string   `json:"id,omitempty"`
+	Message   *int      `json:"message,omitempty"`
+	Messages  *[]int    `json:"messages,omitempty"`
 }
 
 type Node struct {
-	NodeId  string
-	LastMsg *Message
-	Stdin   io.Reader
-	Stdout  io.Writer
+	NodeId   string
+	LastMsg  *Message
+	Stdin    io.Reader
+	Stdout   io.Writer
+	messages []int
 }
 
 func NewNode() *Node {
 	return &Node{
-		Stdin:  os.Stdin,
-		Stdout: os.Stdout,
+		Stdin:    os.Stdin,
+		Stdout:   os.Stdout,
+		messages: []int{},
 	}
 }
 
-func (n *Node) init(msg *Message) {
-	n.NodeId = *msg.Body.NodeId
+func (n *Node) res(msg *Message, body *MessageBody) error {
 	n.LastMsg = msg
+	body.InReplyTo = n.LastMsg.Body.MsgId
 
 	res := Message{
 		Src:  n.NodeId,
 		Dest: n.LastMsg.Src,
-		Body: MessageBody{
-			Type:      "init_ok",
-			InReplyTo: n.LastMsg.Body.MsgId,
-		},
+		Body: *body,
 	}
 
 	json.NewEncoder(n.Stdout).Encode(res)
+	return nil
+}
+
+func (n *Node) init(msg *Message) {
+	n.NodeId = *msg.Body.NodeId
+	n.res(msg, &MessageBody{
+		Type: "init_ok",
+	})
 }
 
 func (n *Node) echo(msg *Message) {
-	n.LastMsg = msg
-
-	reply := Message{
-		Src:  n.NodeId,
-		Dest: msg.Src,
-		Body: MessageBody{
-			Type:      "echo_ok",
-			InReplyTo: msg.Body.MsgId,
-			Echo:      msg.Body.Echo,
-		},
-	}
-
-	json.NewEncoder(n.Stdout).Encode(reply)
+	n.res(msg, &MessageBody{
+		Type: "echo_ok",
+		Echo: msg.Body.Echo,
+	})
 }
 
 func (n *Node) generate(msg *Message) {
-	n.LastMsg = msg
 	id := fmt.Sprintf("id-%s-%d", n.NodeId, *msg.Body.MsgId)
+	n.res(msg, &MessageBody{
+		Type: "generate_ok",
+		Id:   &id,
+	})
+}
 
-	reply := Message{
-		Src:  n.NodeId,
-		Dest: msg.Src,
-		Body: MessageBody{
-			Type:      "generate_ok",
-			InReplyTo: msg.Body.MsgId,
-			Id:        &id,
-		},
-	}
+func (n *Node) broadcast(msg *Message) {
+	n.messages = append(n.messages, *msg.Body.Message)
+	n.res(msg, &MessageBody{
+		Type: "broadcast_ok",
+	})
+}
 
-	json.NewEncoder(n.Stdout).Encode(reply)
+func (n *Node) read(msg *Message) {
+	n.res(msg, &MessageBody{
+		Type:     "read_ok",
+		Messages: &n.messages,
+	})
+}
+
+func (n *Node) topology(msg *Message) {
+	n.res(msg, &MessageBody{
+		Type:      "topology_ok",
+		InReplyTo: msg.Body.MsgId,
+	})
 }
 
 func (n *Node) Run() error {
@@ -99,12 +111,20 @@ func (n *Node) Run() error {
 			if msg.Body.Type == "init" {
 				n.init(&msg)
 			}
-
 			if msg.Body.Type == "echo" {
 				n.echo(&msg)
 			}
 			if msg.Body.Type == "generate" {
 				n.generate(&msg)
+			}
+			if msg.Body.Type == "broadcast" {
+				n.broadcast(&msg)
+			}
+			if msg.Body.Type == "read" {
+				n.read(&msg)
+			}
+			if msg.Body.Type == "topology" {
+				n.topology(&msg)
 			}
 		}()
 	}
